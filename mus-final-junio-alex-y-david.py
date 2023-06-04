6f222288cda0a583cc13f37337a8645968948d21
RegresoaSinnoh <davidreq@ucm.es>
2023-06-04 13:55:56 +0200

    04-junio 14:00

--- mus-final-junio-alex-y-david.py
+++ mus-final-junio-alex-y-david.py
@@ -38,7 +38,8 @@ class Fm:
         for i in range(len(self.listaFreqs)-1,-1,-1):
             samples = self.listaFreqs[i][1] * np.sin(2*np.pi*self.listaFreqs[i][0]*currChunk/self.bitRate + samples)
         
-        self.currPos += self.chunkSize
+        #TODO: ver que pasa con esta linea
+        #self.currPos += self.chunkSize
         return samples * self.vol
     
 class Partitura: 
@@ -67,9 +68,94 @@ class Partitura:
 
         self.currPos += self.chunkSize
         return currNotas
-            
 
+class AbcInput:
+    def __init__(self):
+        return
+
+    def setIndiceMelodia(self, X): #1, 2, 3...
+        self.IndiceMelodia = X
+
+    def setTitulo(self, T):
+        self.titulo = T
+
+    def setCompas(self, M): #2/4, 3/4, 4/4, 6/8, ... 
+        self.compas = M
+
+    def setDuracionNotaPorDefecto(self, L): #si no se indica lo contrario, esta sera la duracion de cualquier nota que se indique (ejemplo: 1/8, 1/4, ...)
+        self.duracionNotaPorDefecto = L
+
+    def setTipoMelodia(self, R):
+        self.tipoMelodia = R
         
+    def setKey(self, K): #clave de sol, de fa...
+        self.key = K
+
+    def setNotas(self, notas):
+        self.notas = notas
+
+    def getNotas(self):
+        return self.notas
+
+def isNum(c):
+    return c.isnumeric()
+
+def getNum(c):
+    return int(c)
+
+def leeArchivo(pathArchivo):
+    file = open(pathArchivo, "r")
+    archivoStr = file.read()
+    lineas = archivoStr.splitlines()
+
+    abc = AbcInput()
+    #            Do       RE       MI       FA       SOL      LA      SI      do'     re'     mi'     fa'     sol'     la'      si'
+    notasExpI = {"C": -9, "D": -7, "E": -5, "F": -4, "G": -2, "A": 0, "B": 2, "c": 3, "d": 5, "e": 7, "f":8 , "g":10 , "a": 12, "b": 14}
+
+    notas = []
+    for linea in lineas:      
+        if (len(linea) >= 3 and linea[1] == ":"): #Headers
+            aux = linea.split(":")
+            parteIzq = aux[0]
+            parteDer = aux[1]
+
+            if (parteIzq == "X"):
+                abc.setIndiceMelodia(parteDer)
+            elif (parteIzq == "T"):
+                abc.setTitulo(parteDer)
+            elif (parteIzq == "M"):
+                abc.setCompas(parteDer)
+            elif (parteIzq == "L"):
+                abc.setDuracionNotaPorDefecto(parteDer)
+            elif (parteIzq == "R"):
+                abc.setTipoMelodia(parteDer)
+            elif (parteIzq == "K"):
+                abc.setKey(parteDer)
+            else:
+                print("---ERROR---"),
+                print("No se ha podido proicesar la linea:")
+                print(linea)
+                print("==========")
+        else:
+            #seccion con notas
+            prevChar = ""
+            for c in linea:
+                if isNum(c):
+                    if prevChar == "|":
+                        prevChar = c
+                        continue #Numero para marcar que compas tocar en una repeticion
+                    else:
+                        freq = notas[-1][0]
+                        notas[-1] = [freq, getNum(c)]
+                if c in notasExpI:
+                    i = notasExpI[c]
+                    freq = 440 * (2 ** (i / 12)) #440 * 2^(i/12)
+                    notas.append([freq,1])
+                prevChar = c
+
+    abc.setNotas(notas)
+    return abc
+
 
         
     
@@ -79,7 +165,7 @@ class Partitura:
 ##[[220, 1][440, 2]], 100
 
 
-def main():
+def main(abc):
     SRATE = 44100       # sampling rate, Hz, must be integer
     CHUNK = 128
     stream = sd.OutputStream(samplerate=SRATE,blocksize=CHUNK,channels=1)  
@@ -89,7 +175,7 @@ def main():
     c = ' '
 
     myFm = Fm(SRATE, CHUNK, vol = 0.05)
-    myPartitura = Partitura(SRATE, CHUNK, [[261.626,1], [293.665, 2], [329.628,1], [349.228, 2]], 60)
+    myPartitura = Partitura(SRATE, CHUNK, abc.getNotas() , 60)
 
 
     # [(fc,vol),(fm1,beta1),(fm2,beta2),...]
@@ -113,4 +199,6 @@ def main():
 
 #Ejecutar el programa:------------------------------------
 if __name__ == "__main__":
-    main()
+    abc = leeArchivo("input.txt")
+    main(abc)
+
